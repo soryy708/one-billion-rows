bdbea5c6cedc7e66e7000a512262bcccd6288cd4
Climax708 <soryy708@gmail.com>
2026-02-19 01:03:17 +0200

    replace fread with mmap

--- src/run/stream-file.c
+++ src/run/stream-file.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stddef.h>
+#include <stdlib.h>
 #include "stream-file.h"
 #include "../signal-handler.h"
 #include "../debug-log.h"
-
-#define BUFFER_SIZE (4 * 1024)
+#include "../panic.h"
+// POSIX Specific:
+#include <sys/mman.h>
+#include <unistd.h>
 
 void streamFile(FILE *file, FileStreamChunkObserver observer)
 {
@@ -12,17 +15,47 @@ void streamFile(FILE *file, FileStreamChunkObserver observer)
     const long lastPosition = ftell(file);
     rewind(file);
 
-    char buffer[BUFFER_SIZE];
-    unsigned int bytesRead;
-    while (((bytesRead = fread(buffer, sizeof(char), BUFFER_SIZE - 1, file)) > 0) && !signalledToStop())
+    unsigned int bufferSize = getStreamFileChunkSize();
+
+    char *str = malloc(sizeof(char) * (bufferSize + 1));
+    if (str == nullptr)
+        return panic("OOM");
+    for (unsigned int i = 0; i < lastPosition / bufferSize && !signalledToStop(); ++i)
     {
-        debugPrintf("Reading file %.1f%%\n", (float)ftell(file) / lastPosition);
-        buffer[bytesRead] = '\0';
-        observer(buffer);
+        debugPrintf("Reading file %.1f%%\n", (i * bufferSize) / lastPosition);
+
+        char *address = mmap(nullptr, bufferSize, PROT_READ, MAP_PRIVATE, fileno(file), i * bufferSize);
+        if (address == MAP_FAILED)
+        {
+            panic("mmap Failed");
+            free(str);
+            return;
+        }
+
+        memcpy(str, address, bufferSize);
+        str[bufferSize] = '\0';
+        observer(str);
+
+        munmap(address, bufferSize);
     }
+    if (!signalledToStop())
+    {
+        unsigned int lastChunkStart = (lastPosition / bufferSize) * bufferSize;
+        if (lastChunkStart != lastPosition)
+        {
+            fseek(file, lastChunkStart, SEEK_SET);
+            unsigned int bytesRead = fread(str, sizeof(char), bufferSize, file);
+            if (bytesRead > 0)
+            {
+                str[bytesRead] = '\0';
+                observer(str);
+            }
+        }
+    }
+    free(str);
 }
 
 const unsigned int getStreamFileChunkSize()
 {
-    return BUFFER_SIZE;
+    return sysconf(_SC_PAGE_SIZE);
 }
