12f084fb1afe532dd22d68c50ce3f279118a23cf
Climax708 <soryy708@gmail.com>
2026-03-07 17:44:29 +0200

    create vector class

---
+++ src/vector.c
@@ -0,0 +1,96 @@
+#include <stdlib.h>
+#include "vector.h"
+#include "c-polyfill.h"
+
+struct Vector
+{
+    size_t allocatedLength;
+    size_t length;
+    void **elements;
+    struct VectorOptions options;
+};
+
+struct Vector *vectorConstructor(struct VectorOptions options)
+{
+    struct Vector *v = malloc(sizeof(struct Vector));
+    if (v == nullptr)
+        return nullptr; // Panic
+    v->allocatedLength = 0;
+    v->length = 0;
+    v->elements = nullptr;
+    v->options = options;
+    return v;
+}
+
+void vectorDeconstructor(struct Vector *v)
+{
+    if (v == nullptr)
+        return;
+    if (v->elements != nullptr)
+        free(v->elements);
+    free(v);
+}
+
+unsigned int vectorLength(struct Vector *v)
+{
+    if (v == nullptr)
+        return 0;
+    return v->length;
+}
+
+void growVector(struct Vector *v)
+{
+    if (v == nullptr)
+        return;
+
+    if (v->elements == nullptr)
+    {
+        v->elements = malloc(v->options.elementSize);
+        if (v->elements == nullptr)
+            return; // Panic
+        v->allocatedLength = 1;
+        return;
+    }
+
+    switch (v->options.growthType)
+    {
+    case QUADRATIC:
+    {
+        void **realloced = realloc(v->elements, v->options.elementSize * 2 * v->allocatedLength);
+        if (realloced == nullptr)
+            return; // Panic
+        v->elements = realloced;
+        v->allocatedLength *= 2;
+        break;
+    }
+    default:
+        return; // Panic
+    }
+}
+
+void vectorPush(struct Vector *v, void *element)
+{
+    if (v == nullptr)
+        return;
+
+    if (v->length >= v->allocatedLength)
+        growVector(v);
+    if (v->length >= v->allocatedLength)
+        return; // Panic, growVector failed
+
+    v->elements[v->options.elementSize * v->length] = element;
+    ++v->length;
+}
+
+void *vectorGet(struct Vector *v, unsigned int index)
+{
+    if (v == nullptr || index >= v->length)
+        return nullptr;
+    return v->elements[v->options.elementSize * index];
+}
+
+void vectorForEach(struct Vector *v, void (*callback)(void *))
+{
+    for (unsigned int i = 0; i < vectorLength(v); ++i)
+        callback(vectorGet(v, i));
+}
---
+++ src/vector.h
@@ -0,0 +1,24 @@
+#ifndef VECTOR_H_INCLUDED
+#define VECTOR_H_INCLUDED
+
+struct Vector;
+
+enum VectorOptionsGrowthType
+{
+    QUADRATIC
+};
+
+struct VectorOptions
+{
+    enum VectorOptionsGrowthType growthType;
+    size_t elementSize;
+};
+
+struct Vector *vectorConstructor(struct VectorOptions options);
+void vectorDeconstructor(struct Vector *v);
+unsigned int vectorLength(struct Vector *v);
+void vectorPush(struct Vector *v, void *element);
+void *vectorGet(struct Vector *v, unsigned int index);
+void vectorForEach(struct Vector *v, void (*callback)(void *));
+
+#endif
